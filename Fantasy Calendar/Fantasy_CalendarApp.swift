dda39299f8dd8109e9c5d5535ba69bd03128381b
Brady Robshaw <heybrady88@gmail.com>
2022-07-11 14:21:52 -0400

    Initial Commit

---
+++ Fantasy Calendar/Fantasy_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fantasy_CalendarApp.swift
+//  Fantasy Calendar
+//
+//  Created by Brady Robshaw on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Fantasy_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
